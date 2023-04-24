350b7f0d170e30a9d526a4a5eeb73d67dd101526
atulya-kairati <atulyakairati@gmail.com>
2023-04-24 12:27:09 +0530

    Added helmet

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 const express = require("express")
+const helmet = require("helmet")
 
 const app = express()
+
+/**
+ * helmet.js adds a lot of security feature such as hsts, and no XSS etc.
+ * Since it is a security middleware, it must be the first middleware
+ * to used. So that all of our requests go through it.
+ */
+app.use(helmet()) 
 
 app.get('/', (req, res) => {
     return res.send("Hello there!")
