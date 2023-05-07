2bbc6f93def474fdccaeeb9a18ac2ff714624ac8
atulya-kairati <atulyakairati@gmail.com>
2023-05-07 23:07:37 +0530

    cookie sessions

--- readme.md
+++ readme.md
@@ -30,3 +30,69 @@
 - Go to [Passportjs.org](passportjs.org).
 - View all strategy -> Search *google oauth2*.
 - Install `npm install passport-google-oauth20`.
+
+
+***
+
+### Setting up cookie session
+
+- Install cookie-session package.
+- Import it.
+```js
+const cookieSession = require('cookie-session')
+```
+
+- Use it before intializing passport middleware.
+```js
+app.use(cookieSession({ // use this before passport, since passport will be using the session
+    name: "session",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // a week till user has to login again
+    keys: [process.env.COOKIE_KEY_1, process.env.COOKIE_KEY_2] // multiple keys to help with rotation in case one of the keys is compromised.
+}))
+```
+- Here:
+  - `name` -> name of the session.
+  - `maxAge` -> after which the session expires.
+  - `keys` -> used to sign the cookies so they can't be tampered with.
+
+#### Hooking up to Passport.js
+
+- `cookie-session` not working with passport 0.6. Use 0.5.
+- Turn on sessions in `passport.authenticate(...)`.
+```js
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/failure',
+        successRedirect: '/',
+        session: true, // to use session
+    }),
+    (req, res) => {
+        console.log("Google callback");
+    }
+)
+```
+
+- Define serialization and deserialization strategies using
+```js
+// save the session to the cookie
+passport.serializeUser((user, done) => {
+    // user --> cookie session
+
+    // we are directly populating the cookie here: not recommended
+    done(null, user) // null means no error
+    // done(error, user) if there was any error
+})
+
+
+// read the session from the cookie
+passport.deserializeUser((obj, done) => {
+    // cookie session --> user
+
+    done(null, obj)
+})
+```
+
+- Use passport session middleware (after intializing passport).
+```js
+app.use(passport.session()) // this is to use sessions
+```
--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ const express = require("express")
 const helmet = require("helmet")
 const passport = require('passport')
 const { Strategy } = require('passport-google-oauth20')
+const cookieSession = require('cookie-session')
 
-// Add add a .env file with CLIENT_ID and CLIENT_SECRET
+// Add add a .env file with CLIENT_ID and CLIENT_SECRET at project root
 require('dotenv').config()
 
 const app = express()
@@ -26,26 +27,48 @@ function verifyCallback(accessToken, refreshToken, profile, done) {
 
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
 
+// save the session to the cookie
+passport.serializeUser((user, done) => {
+    // user --> cookie session
+
+    // we are directly populating the cookie here: not recommended
+    done(null, user) // null means no error
+    // done(error, user) if there was any error
+})
+
+
+// read the session from the cookie
+passport.deserializeUser((obj, done) => {
+    // cookie session --> user
+
+    done(null, obj)
+})
+
 /**
  * helmet.js adds a lot of security feature such as hsts, and no XSS etc.
  * Since it is a security middleware, it must be the first middleware
  * to used. So that all of our requests go through it.
  */
-app.use(helmet())
-
+app.use(helmet()) // always on top
+app.use(cookieSession({ // use this before passport, since passport will be using the session
+    name: "session",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // a week till user has to login again
+    keys: [process.env.COOKIE_KEY_1, process.env.COOKIE_KEY_2] // multiple keys to help with rotation in case one of the keys is compromised.
+}))
 app.use(passport.initialize())
+app.use(passport.session()) // this is to use sessions
 
 // Middleware
 const checkIfLoggedIn = (req, res, next) => {
 
     let loggedIn = false
 
+    // TODO: Write logic to detect if user is logged in
+
     if (loggedIn) return next()
 
     return res.status(401).json({ error: "Login required" })
 }
-
-
 
 app.get('/', (req, res) => {
     return res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'))
@@ -68,7 +91,7 @@ app.get('/auth/google/callback',
     passport.authenticate('google', {
         failureRedirect: '/failure',
         successRedirect: '/',
-        session: false,
+        session: true, // to use session
     }),
     (req, res) => {
         console.log("Google callback");
@@ -91,7 +114,9 @@ app.get('/auth/logout', (req, res) => {
 })
 
 
-app.get('/failure', (req, res) => { })
+app.get('/failure', (req, res) => {
+    return res.json({ err: "Failed to authenticate." })
+})
 
 
 app.use('*', (req, res) => {
